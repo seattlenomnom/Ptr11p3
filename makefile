b65a98986793cf420a6dfd2db1084c244bbdb4a0
seattlenomnom <mark.crapser@gmail.com>
2023-07-04 22:20:02 -0700

    initial commit

---
+++ makefile
@@ -0,0 +1,4 @@
+# makefile for ptr11p3
+#
+ptr11p3: ptr11p3.c
+	gcc -g ptr11p3.c -o ./Build/ptr11p3
---
+++ ptr11p3.c
@@ -0,0 +1,63 @@
+/* sourcefile for ptr11p3.
+ *
+ * This is a mule to observe the interaction between a list head and the
+ * function insertEntry.
+ *
+*/
+
+
+/* includes */
+
+#include <stdio.h>
+
+/* defines */
+
+
+
+
+/* structure declarations */
+
+struct entry{
+    int value;
+    struct entry *next;
+};
+
+
+
+
+/* function declarations */
+
+void insertEntry(struct entry *newEntry, struct entry *existEntry);
+
+
+/* main */
+
+int main(int argc, char *argv[]){
+
+
+
+
+
+    return(0);
+}
+
+
+/* function definitions */
+
+void insertEntry(struct entry *newEntry, struct entry *existEntry){
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
